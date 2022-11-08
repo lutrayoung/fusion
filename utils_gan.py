eb53ee5fcdf1ff418128421f7a418b41f8537950
lutrayoung <117271775+lutrayoung@users.noreply.github.com>
2022-11-08 10:25:05 +0800

    Add files via upload

---
+++ utils_gan.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*-
+"""
+Scipy version > 0.18 is needed, due to 'mode' option from scipy.misc.imread function
+"""
+
+import os
+import glob
+import h5py
+import scipy.misc
+import scipy.ndimage
+import argparse
+import numpy as np
+import torch
+from torch import nn
+import cv2
+#import imageio
+
+def read_data(path):
+    """
+    Read h5 format data file
+
+    Args:
+      path: file path of desired file
+      data: '.h5' file format that contains train data values
+      label: '.h5' file format that contains train label values
+    """
+    with h5py.File(path, 'r') as hf:
+        data = np.array(hf.get('data'))
+        label = np.array(hf.get('label'))
+        return data, label
+
+
+
+# 提取图片地址
+def prepare_data(dataset):
+    """
+    Args:
+      dataset: choose train dataset or test dataset
+
+      For train dataset, output data would be ['.../t1.bmp', '.../t2.bmp', ..., '.../t99.bmp']
+    """
+     # 调动训练集
+    filenames = os.listdir(dataset)
+    data_dir = os.path.join(os.getcwd(), dataset)
+    data = glob.glob(os.path.join(data_dir, "*.bmp"))  # 查找bmp文件，存入列表中
+    data.extend(glob.glob(os.path.join(data_dir, "*.tif")))
+    # 将图片按序号排序
+    data.sort(key=lambda x: int(x[len(data_dir) + 1:-4]))
+
+    # print(data)
+    return data
+
+# 将文件以h5py的形式保存
+def make_data(data, label, data_dir):
+    """
+    Make input data as h5 file format
+    Depending on 'is_train' (flag value), savepath would be changed.
+    """
+
+    savepath = os.path.join('.', os.path.join('checkpoint', data_dir, 'train.h5'))
+    if not os.path.exists(os.path.join('.', os.path.join('checkpoint', data_dir))):
+        os.makedirs(os.path.join('.', os.path.join('checkpoint', data_dir)))
+
+    with h5py.File(savepath, 'w') as hf:
+        hf.create_dataset('data', data=data)
+        hf.create_dataset('label', data=label)
+
+
+# 将图像数据保存为文件
+def imread(path, is_grayscale=True):
+    """
+    Read image using its path.
+    Default value is gray-scale, and image is read by YCbCr format as the paper said.
+    """
+    if is_grayscale:
+        # flatten=True 以灰度图的形式读取
+        return scipy.misc.imread(path, flatten=True, mode='YCbCr').astype(np.float)
+    else:
+        return scipy.misc.imread(path, mode='YCbCr').astype(np.float)
+
+
+
+
+
+def input_setup(opt,data_dir,index=0):
+    """
+    Read image files and make their sub-images and saved them as a h5 file format.
+    """
+    # Load data path
+
+    # 取到所有的原始图片的地址
+    data = prepare_data(dataset=data_dir)
+
+
+    sub_input_sequence = []
+    sub_label_sequence = []
+    padding = int(abs(opt.img_size - opt.label_size) / 2)  # 6
+
+
+    for i in range(len(data)):
+        # input_, label_ = preprocess(data[i], opt.scale)
+        input_ = (imread(data[i]) - 127.5) / 127.5
+        label_ = input_
+
+        if len(input_.shape) == 3:
+            h, w, _ = input_.shape
+        else:
+            h, w = input_.shape
+        # 按14步长采样小patch
+        for x in range(0, h - opt.img_size + 1, opt.stride):
+            for y in range(0, w - opt.img_size + 1, opt.stride):
+                sub_input = input_[x:x + opt.img_size, y:y + opt.img_size]  # [33 x 33]
+                # 注意这里的padding，前向传播时由于卷积是没有padding的，所以实际上预测的是测试patch的中间部分
+                sub_label = label_[x + padding:x + padding + opt.label_size,
+                            y + padding:y + padding + opt.label_size]  # [21 x 21]
+
+                sub_input_sequence.append(sub_input)
+                sub_label_sequence.append(sub_label)
+    """
+    len(sub_input_sequence) : the number of sub_input (33 x 33 x ch) in one image
+    (sub_input_sequence[0]).shape : (33, 33, 1)
+    """
+    # Make list to numpy array. With this transform
+    arrdata = np.asarray(sub_input_sequence)  # [?, 33, 33, 1]
+    arrlabel = np.asarray(sub_label_sequence)  # [?, 21, 21, 1]
+    # print(arrdata.shape)
+    make_data(arrdata, arrlabel, data_dir)  # 保存数据
+
+    # if not opt.is_train:
+    #     print(nx, ny)
+    #     print(h_real, w_real)
+    #     return nx, ny, h_real, w_real
+
+
+def imsave(image, path):
+    return scipy.misc.imsave(path, image)
+
+
+# 将一组图片进行组合
+def merge(images, size):
+    h, w = images.shape[1], images.shape[2]
+    img = np.zeros((h * size[0], w * size[1], 1))
+    for idx, image in enumerate(images):
+        i = idx % size[1]
+        j = idx // size[1]
+        img[j * h:j * h + h, i * w:i * w + w, :] = image
+    return (img * 127.5 + 127.5)
+
+
